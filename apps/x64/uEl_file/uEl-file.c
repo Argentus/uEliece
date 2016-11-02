e5964b41fb2095663a46aef6b09458cb7b49d30d
Argentus <radobezak@gmail.com>
2016-11-02 16:55:56 +0100

    update uEl_file

--- apps/x64/uEl_file/uEl-file.c
+++ apps/x64/uEl_file/uEl-file.c
@@ -4,9 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "../../../libuEliece/All/uEliece.h"
-
-#include "uEl_keys.h"
+#include "../../../libuEliece/src/uEliece.h"
 
 int main(int argc, char *argv[]) {
 
@@ -15,42 +13,50 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
+	uEl_PubKey public_key;
+	uEl_PrivKey private_key;
+
 	uint8_t mode = -1;
-	if (argv[1][1]=='e')
+	if (argv[1][1]=='e') {
 		mode = 0;
-	else if (argv[1][1]=='d')
+		FILE* pubkey_file = fopen("uEl_pub.key", "rb");
+		if (pubkey_file == NULL)
+			return -1;
+		if (fread( public_key, sizeof(uint8_t), (UEL_MDPC_M/8)+1, pubkey_file) != (UEL_MDPC_M/8)+1)
+			return -1;
+		fclose(pubkey_file);
+	}
+	else if (argv[1][1]=='d') {
 		mode = 1;
+		FILE* privkey_file = fopen("uEl_priv.key", "rb");
+		if (fread( private_key[0], sizeof(uint16_t), UEL_MDPC_W/2, privkey_file) != UEL_MDPC_W/2)
+			return -1;
+		if (fread( private_key[1], sizeof(uint16_t), UEL_MDPC_W/2, privkey_file) != UEL_MDPC_W/2)
+			return -1;
+		fclose(privkey_file);
+	}
 	else {
 		printf("Usage: uEl-file <-e | -d> <input file> <output file>\n");
 		return -1;
 	}
 
-		printf("g\n"); fflush(stdout);
 	FILE* infile = fopen(argv[2],"rb");
-		printf("g\n"); fflush(stdout);
 	fseek(infile, 0, SEEK_END);
-		printf("g\n"); fflush(stdout);
 	long file_size = ftell(infile);
-		printf("g\n"); fflush(stdout);
 	fseek(infile, 0, SEEK_SET);
-		printf("g\n"); fflush(stdout);
-	
 	uint8_t *msg = malloc(file_size);
-		printf("g\n"); fflush(stdout);
 	fread(msg, file_size, 1, infile);
-		printf("g\n"); fflush(stdout);
 	fclose(infile);
-		printf("g\n"); fflush(stdout);
-	uint32_t length;
+
+	uEl_msglen_t length;
 
 	if (mode==0) {
 		printf("Encrypting\n"); fflush(stdout);
-		uEliece_encrypt( &msg, file_size*8, &length, public_key );
+		uEliece_encrypt( &msg, file_size*8, &length, public_key, uEl_default_rng() );
 	} else {
 		if (uEliece_decrypt( &msg, file_size*8, &length, private_key ) & UEL_BAD_INTEGRITY)
-			printf("WRONG\n");
+			printf("BAD INTEGRITY\n");
 	}
-
 	FILE* outfile = fopen(argv[3],"wb");
 	fwrite(msg, length, 1, outfile);
 	fclose(outfile);
